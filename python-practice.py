5dc34da11180ad30128ec495b91aba35a465d58c
Ismael Pérez Martín <ismaelprzm@gmail.com>
2018-01-24 16:59:35 +0100

    Añado una solucion mejor, en la que se pueden obtener matrices de una dimension dada con cualquier combinacion en bloque de la matriz

--- python-practice.py
+++ python-practice.py
@@ -2,23 +2,46 @@
 
 def main():
     sopa = ([6,2,9,5,9], [2,9,6,7,8], [4,2,8,8,7], [2,2,7,4,2], [2,2,3,2,2])
-    encuentra_maximo(sopa)
+    print (encuentra_maximo(sopa))
 
 def encuentra_maximo(sopa):
     maximos = []
-    for i in range(3):
-        matriz = to_matriz(sopa, i)
-        print (matriz)
-        maximos.append(encuentra_submaximos(matriz))
+    #matriz = to_matriz(sopa,1,0,len(sopa))
+    for k in range (3):
+        maximosAux = []
+        for i in range(k+1):
+            for j in range(k+1):
+                matriz = to_matriz(sopa,i,j,len(sopa)-k)
+                maximosAux.extend(encuentra_submaximos(matriz))
+        maximosAux = list(set(maximosAux))
+        maximos.append(tuple(tres_maximos(quick_sort(maximosAux))))
 
     return tuple(maximos)
+
+def tres_maximos(lista):
+    if len(lista)<4:
+        return lista
+    else:
+        return lista[0:3]
 
 def encuentra_submaximos(matriz):
     impares = []
     for i in range(len(matriz)):
-        n = 0
-        for j in range (len(matriz[i])):
-            n += matriz[i][j] *(10 ** len(matriz[i])-j)
+        n = int(''.join(str(j) for j in matriz[i]))
+
+        n_invertido = int(str(n)[::-1])
+
+        if n%2 != 0:
+            impares.append(n)
+        if n_invertido%2 != 0:
+            impares.append(n_invertido)
+
+    for j in range(len(matriz)):
+        aux = []
+        for i in range(len(matriz)):
+            aux.append(matriz[i][j])
+
+        n = int(''.join(str(j) for j in aux))
 
         n_invertido = int(str(n)[::-1])
 
@@ -29,29 +52,27 @@ def encuentra_submaximos(matriz):
 
     impares_ordenados = quick_sort(impares)
 
-    if len(impares_ordenados)<4:
-        return tuple(impares)
-    else:
-        return tuple(impares[len(impares)-3:])
+    print(impares_ordenados)
+
+    return tres_maximos(impares_ordenados)
 
 
 # Devuelve la lista de tuplas como una matriz, con su dimension reducida en n unidades
-def to_matriz(lista, n):
-    tam = len(lista)-n
+def to_matriz(lista, fIni, cIni, tam):
     # Inicializamos la matriz
     matriz = []
-    for i in range(tam):
+    for i in range(fIni, fIni+tam):
         matriz.append([])
-        for j in range(tam):
-            matriz[i].append(None)
-            matriz[i][j] = lista[i][j]
-            print (matriz[i][j], end="")
+        for j in range(cIni, cIni+tam):
+            matriz[i-fIni].append(None)
+            matriz[i-fIni][j-cIni] = lista[i][j]
+            print (matriz[i-fIni][j-cIni], end="")
         print("")
     print("\n")
 
     return matriz
 
-
+# Ordena una lista de elementos de mayor a menor
 def quick_sort(array):
     less = []
     equal = []
@@ -71,7 +92,7 @@ def quick_sort(array):
                 greater.append(x)
 
         # Devuelvo la concatenacion de los arrays ordenador less + equal + greater
-        return quick_sort(less)+equal+quick_sort(greater)
+        return quick_sort(greater)+equal+quick_sort(less)
     else:
         return array
 
